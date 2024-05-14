35597583e1d24c797f4a5c3f06b34a37f9195953
Joshua Blais <josh@joshblais.com>
2024-05-14 07:44:52 -0600

    add hop keybinds

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,14 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "kj", "<ESC>")
 
+-- place this in one of your configuration file(s)
+local hop = require('hop')
+local directions = require('hop.hint').HintDirection
+vim.keymap.set('', 'f', function()
+  hop.hint_char2({ direction = directions.AFTER_CURSOR, current_line_only = false })
+end, {remap=true})
+vim.keymap.set('', 'F', function()
+  hop.hint_char2({ direction = directions.BEFORE_CURSOR, current_line_only = false })
+end, {remap=true})
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
