580f0b37fb0e1be09f49565247bda5cc374ab4d8
Joshua Blais <josh@joshblais.com>
2024-05-14 07:19:28 -0600

    fix line breaks

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,12 @@ require "nvchad.options"
 
 -- add yours here!
 vim.opt.relativenumber = true
+vim.opt.spelllang = 'en_us'
+vim.opt.spell = true
+-- Line wrapping
+vim.opt.textwidth = 80
+vim.opt.wrap = true
+vim.opt.linebreak = true
+
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
