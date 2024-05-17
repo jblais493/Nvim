d9d225386dd8ae791792c685457382192b9320bb
Joshua Blais <josh@joshblais.com>
2024-05-17 12:02:20 -0600

    fix issue with indentblankline

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -77,12 +77,9 @@ return {
 }},
   {
     -- Add indentation guides even on blank lines
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = true
-    },
+"lukas-reineke/indent-blankline.nvim",
+main = "ibl",
+commit = "29be0919b91fb59eca9e90690d76014233392bef",
   },
 
   {
