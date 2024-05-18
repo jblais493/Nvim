c3fe1e1b8552adc10dff8ab132c2199ee8788ef9
Joshua Blais <josh@joshblais.com>
2024-05-18 10:09:27 -0600

    add in follow link for obsidian

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -121,6 +121,23 @@ return {
       attachments = {
         img_folder = "assets/imgs",
       },
+      -- Optional, for templates (see below).
+  templates = {
+    folder = "templates",
+    date_format = "%Y-%m-%d",
+    time_format = "%H:%M",
+    -- A map for custom variables, the key should be the variable and the value a function
+    substitutions = {},
+  },
+
+  -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
+  -- URL it will be ignored but you can customize this behavior here.
+  ---@param url string
+  follow_url_func = function(url)
+    -- Open the URL in the default web browser.
+    -- vim.fn.jobstart({"open", url})  -- Mac OS
+    vim.fn.jobstart({"xdg-open", url})  -- linux
+  end,
     },
   },
 
