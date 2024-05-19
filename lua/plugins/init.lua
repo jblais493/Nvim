6ed1afd660b566ed9f4da8cdb24f4c3adad0e8f8
Joshua Blais <josh@joshblais.com>
2024-05-19 10:12:13 -0600

    obsidian stuff

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -122,7 +122,19 @@ return {
     -- A map for custom variables, the key should be the variable and the value a function
     substitutions = {},
   },
+  -- Where to put new notes. Valid options are
+  --  * "current_dir" - put new notes in same directory as the current buffer.
+ --  * "notes_subdir" - put new notes in the default notes subdirectory.
+  new_notes_location = "notes_subdir",
+ -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
+  completion = {
+    -- Set to false to disable completion.
+    nvim_cmp = true,
+    -- Trigger completion at 2 chars.
+    min_chars = 2,
+  },
 
+  wiki_link_func = "use_alias_only",
   -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
   -- URL it will be ignored but you can customize this behavior here.
   ---@param url string
