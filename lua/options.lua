9817de7aed618ca27348cbab07bf59d9924e9438
Joshua Blais <josh@joshblais.com>
2024-05-19 09:45:53 -0600

    addin folding

--- lua/options.lua
+++ lua/options.lua
@@ -9,5 +9,12 @@ vim.opt.wrap = true
 vim.opt.linebreak = true
 vim.filetype.add({ extension = { templ = "templ" } })
 
+-- Enable folding based on syntax
+vim.o.foldmethod = 'syntax'
+-- Set fold level to 1 by default
+vim.o.foldlevel = 2 
+-- Use Markdown syntax for folding
+vim.g.markdown_folding = 1
+
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
