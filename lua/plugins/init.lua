ce9facd1f6af6e2e33294eb173f64cc2df4f7df1
Joshua Blais <josh@joshblais.com>
2024-07-17 09:52:30 -0700

    addin snippets

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -177,6 +177,54 @@ return {
   },
 },
 
+-- LuaSnip config
+{
+  "L3MON4D3/LuaSnip",
+  config = function()
+    local luasnip = require("luasnip")
+    
+    -- Load snippets from ~/.config/nvim/snippets/
+    require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/snippets/"})
+    
+    -- Extend .templ files to use HTML snippets
+    luasnip.filetype_extend("templ", {"html"})
+    
+    -- Optional: Add .templ to the list of file types that trigger HTML LSP
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "templ",
+      callback = function()
+        local client = vim.lsp.start({
+          name = "html-lsp",
+          cmd = {"html-languageserver", "--stdio"},
+          root_dir = vim.fn.getcwd(),
+        })
+        if not client then
+          vim.notify("Failed to start HTML LSP for templ file", vim.log.levels.WARN)
+        end
+      end,
+    })
+
+    -- Set up basic LuaSnip options
+    luasnip.config.set_config({
+      history = true,
+      updateevents = "TextChanged,TextChangedI",
+    })
+
+    -- Lazy load snippets
+    luasnip.loaders.from_lua.lazy_load()
+
+    -- Unlink snippets when leaving insert mode
+    vim.api.nvim_create_autocmd("InsertLeave", {
+      callback = function()
+        if luasnip.session.current_nodes[vim.api.nvim_get_current_buf()]
+            and not luasnip.session.jump_active
+        then
+          luasnip.unlink_current()
+        end
+      end,
+    })
+  end,
+},
 
 {
   "folke/noice.nvim",
