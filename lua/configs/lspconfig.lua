9565e122d68022ee4b7d7fa6c1403a7a83f5e8e4
Joshua Blais <josh@joshblais.com>
2024-10-29 07:57:48 -0600

    fix gopls and floating func details

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,11 +1,9 @@
--- EXAMPLE 
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
-
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
 
+local servers = { "html", "cssls" }
 -- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -14,6 +12,21 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
+
+-- explicit gopls setup
+lspconfig.gopls.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+      },
+      staticcheck = true,
+    },
+  },
+}
 
 -- typescript
 lspconfig.ts_ls.setup {
